f22085b38cac4678c80533b43ffc966989e2c4cb
QeNo <moodiiy.98@gmail.com>
2022-02-02 01:46:12 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
 import Home from './pages/Home/Home.component';
+import About from './pages/About/About.component';
+import MakeCv from './pages/MakeCv/MakeCv.component';
+// Components
+import Navbar from './components/Navbar/Navbar.component';
 
 function App() {
     return (
         <div className='App'>
-            <Home />
+            <Navbar />
+
+            {/* Route */}
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='about' element={<About />} />
+                <Route path='makecv' element={<MakeCv />} />
+            </Routes>
         </div>
     );
 }
