005690f32f341f50dd4a724161f5262f545b0579
QeNo <moodiiy.98@gmail.com>
2022-02-02 01:48:19 +0200

    add about

--- src/pages/About/About.component.jsx
+++ src/pages/About/About.component.jsx
@@ -1,5 +1,13 @@
 const About = () => {
-    return <h1>About</h1>;
+    return (
+        <div className='about'>
+            <div className='container'>
+                <div className='about-header text-center bg-primary'>
+                    <h1>About</h1>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default About;
