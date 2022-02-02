f8b537d4e58ca109352011d8bb6a13ebba870944
QeNo <moodiiy.98@gmail.com>
2022-02-03 00:49:50 +0200

    add navigate router and signup, sign in pages to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages
 import Home from './pages/Home/Home.component';
 import About from './pages/About/About.component';
 import MakeCv from './pages/MakeCv/MakeCv.component';
+import SignUp from './pages/SignUp/SignUp.component';
+import SignIn from './pages/SignIn/SignIn.component';
+
 // Components
 import Navbar from './components/Navbar/Navbar.component';
 import Footer from './components/Footer/Footer.component';
 
+// auth State
+
 function App() {
+    const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
+    const userInfo = JSON.parse(localStorage.getItem('UserInfo'));
+
     return (
         <div className='App'>
-            <Navbar />
+            <Navbar isLoggedIn={isLoggedIn} userInfo={userInfo} />
 
             {/* Route */}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='about' element={<About />} />
                 <Route path='makecv' element={<MakeCv />} />
+                <Route path='*' element={<h1>404</h1>} />
+                <Route
+                    path='signin'
+                    element={isLoggedIn ? <Navigate to='/' /> : <SignIn />}
+                />
+                <Route
+                    path='signup'
+                    element={isLoggedIn ? <Navigate to='/' /> : <SignUp />}
+                />
             </Routes>
+            {/* Route */}
 
             <Footer />
         </div>
