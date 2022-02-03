0d2bb78ba2a4908045dfbabd28dcb6046138c536
QeNo <moodiiy.98@gmail.com>
2022-02-04 00:14:02 +0200

    add form component

--- src/components/Form/Form.component.jsx
+++ src/components/Form/Form.component.jsx
@@ -1,5 +1,26 @@
+import './Form.style.scss';
+
 const Form = () => {
-    return <form></form>;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div class='form-style form-style-two'>
+            <form onsubmit={handleSubmit}>
+                <div class='row'>
+                    <div class='col-lg'>
+                        <div class='form-input mt-5'>
+                            <label>Enter userid</label>
+                            <div class='input-items default'>
+                                <input type='text' placeholder='Default' />
+                                <i class='lni lni-user'></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Form;
